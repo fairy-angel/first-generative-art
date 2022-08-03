dcdfddfb92addda497c35777939c3c4aa36b3cee
fairy-angel <fairy.angelic113@gmail.com>
2022-08-03 15:26:42 -0400

    start of fractal branch

--- fractals/script.js
+++ fractals/script.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', function(){
     const canvas =document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    canvas.width =window.innerWidth * 0.8;
-    canvas.height=window.innerHeight * 0.8;
+    canvas.width =window.innerWidth;
+    canvas.height=window.innerHeight;
 //canvas settings
     ctx.fillStyle= 'blue';
     ctx.strokeStyle = 'pink'
@@ -12,33 +12,42 @@ window.addEventListener('load', function(){
     //effect settings
     let size = 200;
     let sides = 5;
-    let maxLevel = 20;
+    let maxLevel = 3;
+    let scale = 0.5;
+    let spread = 0.8;
+    let branches = 2;
     ctx.save();
     ctx.translate(canvas.width/2,canvas.height/2);
     ctx.scale(1,1);
     ctx.rotate(0);
   //  ctx.fillRect(0,0,canvas.width,canvas.height);
-
   function drawBranch(level){
     if(level > maxLevel) return; 
-    ctx.beginPath();
-    ctx.moveTo(0,0);
-    ctx.lineTo(size,0); 
-    ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(0,0);
+      ctx.lineTo(size,0); 
+      ctx.stroke();
+    for (let i = 0; i < branches; i++){
+      ctx.save();
+      ctx.translate(size - (size/branches) *i,0); 
+      ctx.rotate(spread);
+      ctx.scale(scale,scale);
+      drawBranch(level + 1);
+      ctx.restore();
+  
+      ctx.save();
+      ctx.translate(size - (size/branches) *i,0); 
+      ctx.rotate(-spread);
+      ctx.scale(scale,scale);
+      drawBranch(level +1);
+      ctx.restore();
 
-    ctx.save();
-    ctx.translate(size/2,0); 
-    ctx.rotate(0.6);
-    ctx.scale(0.9,0.9);
-    drawBranch(level +1)
-    ctx.restore;
+    }
   }
     drawBranch(0);
   /*  for(let i = 0; i < sides; i++){
-
     ctx.rotate((Math.PI * 2)/sides);
   }
   */
   ctx.restore();
-
 });
